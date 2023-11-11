9a68f7b203bf16abe52b16ca1fac62fd60719ed5
DioMain <dima123.zample@gmail.com>
2023-11-11 18:10:09 +0300

    project created

---
+++ Medkit/src/test/java/com/medkit/MedkitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedkitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
